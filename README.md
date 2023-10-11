cec8bc070b455d17ab7aeaddd36170f90aaa3276
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-10-11 15:56:18 +0200

    ADT1061: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: aidata Model: ADT1061 Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for AIDATA ADT1061 (ADT1061)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_ADT1061-user
+add_lunch_combo omni_ADT1061-userdebug
+add_lunch_combo omni_ADT1061-eng
